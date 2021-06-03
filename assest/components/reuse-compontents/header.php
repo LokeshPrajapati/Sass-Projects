89dd2a2e4c39fd4a959b4f2c9ea97fca245986ae
LokeshPrajapati <lokeshprajapati000@gmail.com>
2021-06-03 19:05:43 +0530

    added some files

---
+++ assest/components/reuse-compontents/header.php
@@ -0,0 +1,34 @@
+<header class="header container-fluid bg-dark">
+    <div class="row">
+        <nav class="navbar d-flex justify-content-between align-items-center w-100">
+            <a href="#" class="navbar-brand mr-16">
+                Logo
+            </a>
+            <button class="btn-toggle btn">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <div class="navbar-collapse">
+                <ul class="nav d-flex justify-content-between align-items-center">
+                    <li class="nav-item ">
+                        <a href="#" class="nav-links active">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-links">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-links">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-links">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-links">Contact Us</a>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+    </div>
+</header>
